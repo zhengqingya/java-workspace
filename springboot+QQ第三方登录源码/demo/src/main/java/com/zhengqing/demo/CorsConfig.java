3b630811e82bb25aac60517b31d175a22c831dec
郑清 <960869719@qq.com>
2019-09-02 23:57:38 +0800

    init

--- springboot+QQ第三方登录源码/demo/src/main/java/com/zhengqing/demo/CorsConfig.java
+++ springboot+QQ第三方登录源码/demo/src/main/java/com/zhengqing/demo/CorsConfig.java
@@ -5,14 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- *  <p> 解决跨域 </p>
+ *  <p> 全局配置解决跨域 </p>
  *
  * @description:
  * @author: zhengqing
  * @date: 2019/9/2 0002 23:53
  */
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // /**: 表示本应用的所有方法都会去处理跨域请求
