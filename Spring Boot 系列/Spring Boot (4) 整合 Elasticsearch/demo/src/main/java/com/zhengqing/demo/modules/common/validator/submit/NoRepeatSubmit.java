cdcfa5482bac0aec37fe36db9ef5a1ff83ab81eb
zhengqingya <960869719@qq.com>
2019-12-27 17:35:51 +0800

    【add】Spring Boot (4) 整合 Elasticsearch

---
+++ Spring Boot 系列/Spring Boot (4) 整合 Elasticsearch/demo/src/main/java/com/zhengqing/demo/modules/common/validator/submit/NoRepeatSubmit.java
@@ -0,0 +1,24 @@
+package com.zhengqing.demo.modules.common.validator.submit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p> 注解：校验表单重复提交 </p>
+ *
+ * @author : zhengqing
+ * @description :
+ * @date : 2019/11/27 9:59
+ */
+// 作用到方法上
+@Target(ElementType.METHOD)
+// 运行时有效
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoRepeatSubmit {
+    /**
+     * 默认时间3秒
+     */
+    int time() default 3 * 1000;
+}
