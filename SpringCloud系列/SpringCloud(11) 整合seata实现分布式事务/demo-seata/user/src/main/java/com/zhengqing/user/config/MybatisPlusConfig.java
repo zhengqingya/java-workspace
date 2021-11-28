543c35103c20d638e00f8a189b2817834d59108a
zhengqingya <960869719@qq.com>
2021-11-28 15:08:08 +0800

    add SpringCloud(11) 整合seata实现分布式事务

---
+++ SpringCloud系列/SpringCloud(11) 整合seata实现分布式事务/demo-seata/user/src/main/java/com/zhengqing/user/config/MybatisPlusConfig.java
@@ -0,0 +1,34 @@
+package com.zhengqing.user.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * <p> MybatisPlus配置类 </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2019/10/8 11:36
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.zhengqing.user.**.mapper*")
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：https://mp.baomidou.com/guide/page.html <br>
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
