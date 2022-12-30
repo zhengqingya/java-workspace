8925660e0666c856cf33ebdb7affa8ddc351201a
zhengqingya <960869719@qq.com>
2022-12-30 18:26:44 +0800

    update code

--- Spring Boot 系列/SpringBoot(56) 整合Easy-Es实现搜索/demo-easy-es/src/main/java/com/zhengqing/demo/api/TestEasyEsController.java
+++ Spring Boot 系列/SpringBoot(56) 整合Easy-Es实现搜索/demo-easy-es/src/main/java/com/zhengqing/demo/api/TestEasyEsController.java
@@ -39,6 +39,14 @@ public class TestEasyEsController {
                             .build()
             );
         }
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("人生是单行路，只有奋斗了，才会有光明的前途。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("过去的永远不再来，未来的还需要努力。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("请不要在该吃苦的年纪选择安逸，走自己的路，为自己的梦想好好去奋斗，即使有人亏待你，时间也不会亏待你，人生更不会亏待你。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("从零开始需要勇气，而我从来不缺少勇气，成长是逼迫自己努力的过程。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("努力是一种生活态度，和年龄无关。生活要有激情，只要你有前进的方向和目标，什么时候开始都不晚。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("生活，不是用来妥协的；日子，不是用来将就的。退缩得越多，表现得越卑微，幸福就会离得越远。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("为努力工作的自我加油，提醒虚荣的自我，它不再被抛弃。相信梦想，梦想会相信你，有一个鸿沟，你不配你的野心，也辜负了苦难。").build());
+//        list.add(Document.builder().id(null).title("zhengqingya"  ).content("别总是很快的否定自己,你是优秀的,值得拥有最特别的。").build());
         this.documentMapper.insertBatch(list);
     }
 
