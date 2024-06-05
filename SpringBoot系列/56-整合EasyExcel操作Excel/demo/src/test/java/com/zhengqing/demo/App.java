dca42b73c017ee971da41d009339e20f802689bb
zhengqingya <960869719@qq.com>
2024-06-06 01:02:05 +0800

    test poi

--- SpringBoot系列/56-整合EasyExcel操作Excel/demo/src/test/java/com/zhengqing/demo/App.java
+++ SpringBoot系列/56-整合EasyExcel操作Excel/demo/src/test/java/com/zhengqing/demo/App.java
@@ -26,7 +26,8 @@ import java.util.List;
  */
 public class App {
 
-    long num = 0L;
+    long sum = 0L; // 记录总数
+    int rowIndex = 1; // 每一个工作页的行数
 
     @Test
     public void test() throws Exception {
@@ -34,14 +35,14 @@ public class App {
         Workbook workbook = new SXSSFWorkbook();
         int page = 1;
         int pageSize = 200000;
-        int rowIndex = 1; //每一个工作页的行数
+
         Row row = null;
         Cell cell = null;
         Sheet sheet = null;
         int count = 0;
 
         // 100w 数据以上打开很慢
-        while (count <= 10) {  //不停地查询
+        while (count < 5) {  //不停地查询
             count++;
             List<User> userList = list();
             if (CollectionUtils.isEmpty(userList)) {  //如果查询不到就不再查询了
@@ -49,27 +50,25 @@ public class App {
             }
 //            if (num % 100000 == 0) {  //每100W个就重新创建新的sheet和标题
             rowIndex = 1;
-            //        在工作薄中创建一个工作表
-            System.err.println("第" + count + "个工作表");
-            sheet = workbook.createSheet("第" + count + "个工作表");
-//        设置列宽
+
+            // 在工作薄中创建一个工作表
+            String sheetName = "第" + count + "个工作表";
+            System.err.println(sheetName);
+            sheet = workbook.createSheet(sheetName);
+            // 设置列宽
             sheet.setColumnWidth(0, 8 * 256);
             sheet.setColumnWidth(1, 12 * 256);
             sheet.setColumnWidth(2, 15 * 256);
-            sheet.setColumnWidth(3, 15 * 256);
-            sheet.setColumnWidth(4, 30 * 256);
-            //            处理标题
+            // 处理标题
             String[] titles = new String[]{"编号", "姓名", "手机号"};
-            //        创建标题行
+            // 创建标题行
             Row titleRow = sheet.createRow(0);
-
             for (int i = 0; i < titles.length; i++) {
                 cell = titleRow.createCell(i);
                 cell.setCellValue(titles[i]);
             }
-//            }
 
-//        处理内容
+            // 处理内容
             for (User user : userList) {
                 row = sheet.createRow(rowIndex);
                 cell = row.createCell(0);
@@ -82,26 +81,21 @@ public class App {
                 cell.setCellValue(user.getSex());
 
                 rowIndex++;
-                num++;
+                sum++;
             }
             page++;// 继续查询下一页
         }
-//            导出的文件名称
-        String filename = "百万数据.xlsx";
-//            设置文件的打开方式和mime类型
-//        ServletOutputStream outputStream = response.getOutputStream();
-//        response.setHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes(), "ISO8859-1"));
-//        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 
         FileOutputStream outputStream = new FileOutputStream("test.xlsx");
         workbook.write(outputStream);
+        System.err.println("总数：" + sum);
     }
 
     private List<User> list() {
-        num++;
         List<User> userList = new ArrayList<>();
         for (int i = 0; i < 1_0000; i++) {
-            userList.add(User.builder().id(num).name(DateUtil.now()).sex("男").build());
+            sum++;
+            userList.add(User.builder().id(sum).name(DateUtil.now()).sex("男").build());
         }
         return userList;
     }
