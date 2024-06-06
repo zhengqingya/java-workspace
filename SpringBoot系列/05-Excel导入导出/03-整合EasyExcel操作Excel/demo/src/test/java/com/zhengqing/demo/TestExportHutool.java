322b85afa1437cf1637d8de96e89fe0146820af0
zhengqingya <960869719@qq.com>
2024-06-07 03:52:39 +0800

    feat excel千万级数据测试

--- SpringBoot系列/05-Excel导入导出/03-整合EasyExcel操作Excel/demo/src/test/java/com/zhengqing/demo/TestExportHutool.java
+++ SpringBoot系列/05-Excel导入导出/03-整合EasyExcel操作Excel/demo/src/test/java/com/zhengqing/demo/TestExportHutool.java
@@ -29,23 +29,58 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class TestExportHutool {
 
-    private long ID = 0;
-    private int SUM = 10_0000;
-    private int ONE_NUM = 1_0000;
-    private long COUNT = NumberUtil.ceilDiv(SUM, ONE_NUM);
-    private int PAGE_NUM = 1;
+    private long ID = 0; // 记录总数
+    private int SUM = 40_0000; // 总数据量
+    private int ONE_DB_PAGE_SIZE = 1_0000; // 一次查库数量
+    private int PAGE_NUM = 1; // 第几页
+    private int TOTAL_PAGES = NumberUtil.ceilDiv(SUM, ONE_DB_PAGE_SIZE); // 总页数
+    private int FILE_DATA_NUM = 10_0000; // 单文件数据量
+    private int SHEET_DATA_NUM = 10_0000; // 单sheet存储数据量
 
     @Test
-    public void test_01() throws Exception {
-        StopWatch stopWatch = new StopWatch("excel导出");
+    public void test_multi() throws Exception {
+        StopWatch stopWatch = new StopWatch("excel导出-多文件");
+
+        // 定义参数
+        int fileNum = NumberUtil.ceilDiv(SUM, FILE_DATA_NUM); // 文件数
+        int fileSheetNum = FILE_DATA_NUM / SHEET_DATA_NUM; // 每个文件的sheet数
+        for (int fileIndex = 1; fileIndex <= fileNum; fileIndex++) {
+            stopWatch.start(StrUtil.format("第{}次", fileIndex));
+
+            log.info("excel-拆分文件 总文件数：{}  处理第{}个文件 ", fileNum, fileIndex);
+            BigExcelWriter writer = ExcelUtil.getBigWriter();
+            for (int fileSheetIndex = 1; fileSheetIndex <= fileSheetNum; fileSheetIndex++) {
+                log.info("excel-拆分文件 总文件数：{}  处理第{}个文件 处理第{}个sheet", fileNum, fileIndex, fileSheetIndex);
+                if (fileSheetIndex == 1) {
+                    writer.renameSheet("第1个sheet");
+                } else {
+                    writer.setSheet(StrUtil.format("第{}个sheet", fileSheetIndex));
+                }
+                writeData(writer);
+            }
+            FileOutputStream fos = new FileOutputStream(StrUtil.format("D:/test-{}.xlsx", fileIndex));
+            writer.flush(fos);
+            writer.close();
+
+            stopWatch.stop();
+        }
+        log.info(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
+    }
+
+    @Test
+    public void test_one() throws Exception {
+        StopWatch stopWatch = new StopWatch("excel导出-单文件");
         //        ExcelWriter writer = ExcelUtil.getBigWriter("D:/test.xlsx", "第一个sheet"); // 得是空文件内容才可以写入
 //        ExcelWriter writer = ExcelUtil.getWriterWithSheet("第一个sheet");
         BigExcelWriter writer = ExcelUtil.getBigWriter();
         writer.renameSheet("第1个sheet");
 
-        while (PAGE_NUM <= COUNT) {
+        while (PAGE_NUM <= TOTAL_PAGES) {
             log.info("第{}页", PAGE_NUM);
             stopWatch.start(StrUtil.format("第{}次", PAGE_NUM));
+            if (PAGE_NUM > 1) {
+                writer.setSheet(StrUtil.format("第{}个sheet", PAGE_NUM));
+            }
             writeData(writer);
             PAGE_NUM++;
             stopWatch.stop();
@@ -56,7 +91,6 @@ public class TestExportHutool {
         fos.close();
         writer.close();  // 关闭writer，释放内存
 
-
         log.info(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
     }
 
@@ -64,16 +98,14 @@ public class TestExportHutool {
         for (int columnIndex = 0; columnIndex < 2; columnIndex++) {
             writer.setColumnWidth(columnIndex, 30);
         }
-
+        // 标题
         writer.addHeaderAlias("id", "ID");
         writer.addHeaderAlias("name", "名称");
         writer.addHeaderAlias("time", "时间");
 
-        writer.setSheet(StrUtil.format("第{}个sheet", PAGE_NUM));
-
         // 数据 -- 查db逻辑
         List<Map<String, Object>> rowList = Lists.newArrayList();
-        for (int i = 0; i < ONE_NUM; i++) {
+        for (int i = 0; i < ONE_DB_PAGE_SIZE; i++) {
             ID++;
             Map<String, Object> map = new LinkedHashMap<>();
             map.put("id", ID);
