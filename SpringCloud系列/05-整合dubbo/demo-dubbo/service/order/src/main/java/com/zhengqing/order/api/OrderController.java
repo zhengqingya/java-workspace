d7a517af4f1d425bfe1c644a8ea7fd6b2cfb5174
zhengqingya <960869719@qq.com>
2024-09-01 17:35:19 +0800

    feat demo-dubbo

--- SpringCloud系列/05-整合dubbo/demo-dubbo/service/order/src/main/java/com/zhengqing/order/api/OrderController.java
+++ SpringCloud系列/05-整合dubbo/demo-dubbo/service/order/src/main/java/com/zhengqing/order/api/OrderController.java
@@ -1,7 +1,11 @@
 package com.zhengqing.order.api;
 
+import cn.hutool.core.util.RandomUtil;
+import com.zhengqing.order.rpc.OrderRpcApi;
 import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/order")
 @Api(tags = {"订单api"})
 public class OrderController {
@@ -26,10 +31,18 @@ public class OrderController {
     @Value("${spring.application.name}")
     private String name;
 
+    @DubboReference
+    private OrderRpcApi orderRpcApi;
+
     @GetMapping("/hello")
     public String hello() {
         // int result = 1 / 0;
         return "《order》:" + name;
+    }
+
+    @GetMapping("/detail")
+    public String detail() {
+        return "《RPC》:" + orderRpcApi.detail(RandomUtil.randomLong());
     }
 
 }
