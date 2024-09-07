77f73965f51255625b270ad41300cf9d852460e8
zhengqingya <960869719@qq.com>
2024-09-07 10:09:11 +0800

    test zk

--- SpringBoot系列/68-整合ZooKeeper/demo/src/test/java/com/zhengqing/demo/ZkClient.java
+++ SpringBoot系列/68-整合ZooKeeper/demo/src/test/java/com/zhengqing/demo/ZkClient.java
@@ -7,6 +7,7 @@ import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ZkClient {
@@ -26,7 +27,11 @@ public class ZkClient {
             client.createNode(path, "Hello Zookeeper".getBytes());
             System.out.println("节点是否存在：" + client.exists(path));
 
-            // 创建临时节点 特性：临时节点在其创建者会话结束时自动删除。因此，如果客户端断开连接且不再重连，则该节点将被自动删除。
+            String two_level_path = path + "/two_level";
+            client.createNodeEphemeral(two_level_path, "Hello".getBytes());
+            System.out.println("二级节点是否存在：" + client.exists(two_level_path));
+
+            // 创建临时节点 特性：临时节点在其创建者会话结束时自动删除。因此，如果客户端断开连接且不再重连，则该节点将被自动删除。 tips: 临时节点不能创建子节点
             client.createNodeEphemeral("/test_tmp_node", "Hello".getBytes());
             System.out.println("临时节点是否存在：" + client.exists("/test_tmp_node"));
 
@@ -39,8 +44,8 @@ public class ZkClient {
 
             ThreadUtil.sleep(2, TimeUnit.SECONDS);
 
-            // 删除节点
-            client.deleteNode(path);
+//            client.deleteNode(path); // 删除指定节点
+            client.deleteAllChildren(path); // 删除指定节点及其下的所有子节点
             System.out.println("节点是否存在：" + client.exists(path));
         } catch (Exception e) {
             e.printStackTrace();
@@ -78,6 +83,14 @@ public class ZkClient {
     public void deleteNode(String path) throws Exception {
         zooKeeper.delete(path, -1);
         System.out.println("Node deleted successfully");
+    }
+
+    public void deleteAllChildren(String parentPath) throws Exception {
+        List<String> children = zooKeeper.getChildren(parentPath, false);
+        for (String child : children) {
+            deleteAllChildren(parentPath + "/" + child); // 递归删除子节点
+        }
+        zooKeeper.delete(parentPath, -1); // 删除父节点，即当前节点
     }
 
     public boolean exists(String path) throws Exception {
