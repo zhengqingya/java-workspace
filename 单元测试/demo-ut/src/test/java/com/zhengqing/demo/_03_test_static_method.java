4cb9f61ba6584488967813466eecc88d6ad843ca
zhengqingya <960869719@qq.com>
2024-12-19 14:17:33 +0800

    feat 单元测试 -- 调用静态方法

--- 单元测试/demo-ut/src/test/java/com/zhengqing/demo/_03_test_static_method.java
+++ 单元测试/demo-ut/src/test/java/com/zhengqing/demo/_03_test_static_method.java
@@ -23,16 +23,43 @@ public class _03_test_static_method {
 
     // ==================== ↓↓↓↓↓↓ 单测case ↓↓↓↓↓↓=======================
 
+    /**
+     * 注：多次使用`Mockito.mockStatic`，会报错：`To create a new mock, the existing static mock registration must be deregistered`
+     * 需要及时关闭，否则其他测试用例执行时会报错
+     */
+
     @Test
-    public void test_mock_static_method() throws Exception {
-        Mockito.mockStatic(TestUtil.class); // 不需要返回值的话，这个就可以模拟静态方法执行了...
+    public void test_mock_static_method_1_01() throws Exception {
+        MockedStatic<TestUtil> mockStatic = Mockito.mockStatic(TestUtil.class);// 不需要返回值的话，这个就可以模拟静态方法执行了...
         // 模拟静态方法返回值
         Mockito.when(TestUtil.getNowTime(Mockito.anyInt())).thenReturn("xxx");
         userService._03_test_static_method();
+        mockStatic.close();
     }
 
     @Test
-    public void test_mock_static_method_2() throws Exception {
+    public void test_mock_static_method_1_02() throws Exception {
+        MockedStatic<TestUtil> mockStatic = Mockito.mockStatic(TestUtil.class);// 不需要返回值的话，这个就可以模拟静态方法执行了...
+        // 模拟静态方法返回值
+        Mockito.when(TestUtil.getNowTime(Mockito.anyInt())).thenReturn("xxx");
+        userService._03_test_static_method();
+        mockStatic.close();
+    }
+
+    // ---------------------------------------------------------------------------------
+
+    @Test
+    public void test_mock_static_method_2_01() throws Exception {
+        // 创建静态方法的模拟对象
+        try (MockedStatic<TestUtil> theMock = Mockito.mockStatic(TestUtil.class)) {
+            // 指定静态方法的返回值
+            theMock.when(() -> TestUtil.getNowTime(Mockito.anyInt())).thenReturn("xxx");
+            userService._03_test_static_method();
+        }
+    }
+
+    @Test
+    public void test_mock_static_method_2_02() throws Exception {
         // 创建静态方法的模拟对象
         try (MockedStatic<TestUtil> theMock = Mockito.mockStatic(TestUtil.class)) {
             // 指定静态方法的返回值
